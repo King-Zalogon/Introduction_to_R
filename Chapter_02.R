fb32ec09decbe151e0f1fa2ab6c4d79333c7b7c5
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-10-24 15:45:33 -0300

    Starting chapter 2 of Introduction to R

---
+++ Chapter_02.R
@@ -0,0 +1,23 @@
+hist(rnorm(10000))
+
+1:100 # generate a range of integers between 1 and 100, including both
+
+3 + 7
+10 - 3
+10 * 3
+10 / 3
+10 ^ 3
+10 %% 3
+10 %/% 3
+
+# Functions
+exp(3)
+sqrt(3)
+log(3)
+log(3, 10)
+log10(3)
+log1p(2)
+log2(10)
+pi
+abs(-10.2)
+abs(3-9)
