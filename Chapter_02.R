87e939cd386d87d71301ddbef66675d9884a237e
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-10-24 16:42:50 -0300

    Basic functions in R

--- Chapter_02.R
+++ Chapter_02.R
@@ -21,3 +21,65 @@ log2(10)
 pi
 abs(-10.2)
 abs(3-9)
+
+
+# Relational n logical Operators
+
+!TRUE
+1 == 2
+1 %in% c(1,2,3)
+'A' != 'A'
+10 > 9 
+10 < 9
+10 >= 9
+10 <= 9
+10 == 9 | 10 > 9
+10 == 10 & 10 > 9
+
+
+# Assigning values to a variable using <- or ->
+NUM <- 2023
+2022 -> lastyear # works both ways
+NUM
+
+print(NUM)
+
+(nextyear <- 2024)
+
+# The equal symbol is only used inside functions
+bs <- 2 + 8
+log(10, base = bs) 
+
+
+# ==== FUNCTIONS ==== #
+x <- c(7,9,3,4,5,3,7,8)
+sd(x) # Standard Deviation
+sd
+var # Variance
+
+data("iris")
+set.seed(123)
+x <- rnorm(30, 2, 1)
+y <- rnorm(30, 5, 3)*x/2
+letter <- c("A", "B", "A", "A")
+
+x |> round(2)
+x %>% round(2)
+mean(x)
+median(x)
+sd(x)
+var(x)
+quantile(x)
+range(x) # Minimum and maximum values within a range
+seq_along(x) # creates a sequence of integers between 1 and x
+sum(x)
+max(x)
+min(x)
+seq(0, 10, 2) # creates a sequence from A to B, with a step of C
+seq(0, 10, length=25)
+rep("Peru", 15)
+paste0("Seventh", "Wonder")
+sort(c("Z", "C", "G", "A"))
+rev(c("Z", "C", "G", "A"))
+unique(letter)
+scale(x)
